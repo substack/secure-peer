58679966a610c6b09181baafb226c44e344fb2e4
James Halliday <mail@substack.net>
2012-11-08 02:24:31 -0800

    replace test with another attempt

--- test/secure.js
+++ test/secure.js
@@ -1,44 +1,33 @@
 var test = require('tap').test;
-var secure = require('secure-peer');
-var through = require('through');
 
+var secure = require('../');
 var peer = {
     a : secure(require('./keys/a.json')),
     b : secure(require('./keys/b.json')),
 };
+var through = require('through');
 
-test('accept a secure connection', function (t) {
+test('accept a connection', function (t) {
     t.plan(1);
-    t.on('end', function () {
-        console.log('...');
-    });
     
     var a = peer.a(function (stream) {
         stream.pipe(through(function (buf) {
-console.dir(buf); 
             this.emit('data', String(buf).toUpperCase());
-        }).pipe(stream));
+        })).pipe(stream);
     });
-    
+
     var b = peer.b(function (stream) {
         var data = '';
-        stream.on('data', function (buf) {
-console.dir(String(buf)); 
-            data += buf;
-        });
+        stream.on('data', function (buf) { data += buf });
         stream.on('end', function () {
-            t.equal(data, 'BEEP BOOP\n');
+            t.equal(data, 'BEEP BOOP');
         });
-        stream.write('beep boop\n');
+        stream.end('beep boop');
     });
-    
-    a.on('identify', function (id) {
-        id.accept();
-    });
-    
+
     b.on('identify', function (id) {
         id.accept();
     });
-    
+
     a.pipe(b).pipe(a);
 });
