03f790b4c42aaedac218fa7ba510d93d3872f2fa
James Halliday <mail@substack.net>
2012-11-07 23:26:47 -0800

    flatten the main handler

--- index.js
+++ index.js
@@ -7,108 +7,113 @@ module.exports = function (keys) {
     var group = 'modp5';
     var dh = crypto.getDiffieHellman(group);
     dh.generateKeys();
+    dh.group = group;
     
     return function (cb) {
-        var buffers = [];
-        var stream, encrypt, decrypt;
-        
-        var sec = header(function (buf) {
-            if (decrypt) {
-                var uf = unframe(stream.id.key, buf);
-                if (!uf) {
-                    stream.destroy();
-                    this.destroy();
-                    return;
-                }
-                var s = decrypt.update(String(uf));
-                stream.emit('data', Buffer(s));
+        return securePeer(dh, keys, cb);
+    }
+};
+
+function securePeer (dh, keys, cb) {
+    var buffers = [];
+    var stream, encrypt, decrypt;
+    
+    var sec = header(function (buf) {
+        if (decrypt) {
+            var uf = unframe(stream.id.key, buf);
+            if (!uf) {
+                stream.destroy();
+                this.destroy();
+                return;
             }
-            else buffers.push(buf)
-        });
+            var s = decrypt.update(String(uf));
+            stream.emit('data', Buffer(s));
+        }
+        else buffers.push(buf)
+    });
+    
+    sec.on('accept', function (ack) {
+        var pub = ack.payload.dh.public;
+        var k = dh.computeSecret(pub, 'base64', 'base64');
         
-        sec.on('accept', function (ack) {
-            var pub = ack.payload.dh.public;
-            var k = dh.computeSecret(pub, 'base64', 'base64');
-            
-            encrypt = crypto.createCipher('aes-256-cbc', k);
-            
-            stream = through(write, end);
-            stream.id = ack;
-            
-            function write (buf) {
-                var s = encrypt.update(String(pad(buf)));
-                sec.emit('data', frame(keys.private, Buffer(s)));
-            }
-            
-            function end () {
-                sec.emit('data', ecrypt.final());
-                sec.emit('end');
-                sec.emit('close');
-            }
-            
-            sec.emit('connection', stream);
-            
-            decrypt = crypto.createDecipher('aes-256-cbc', k);
-            
-            buffers.forEach(function (buf) {
-                var uf = unframe(stream.id.key, buf);
-                if (!uf) {
-                    stream.destroy();
-                    sec.destroy();
-                    return;
-                }
-                stream.emit('data', decrypt.update(uf));
-            });
-            buffers = undefined;
-        });
+        encrypt = crypto.createCipher('aes-256-cbc', k);
         
-        sec.once('header', function (meta) {
-            var payload = JSON.parse(meta.payload);
-            
-            var v = verify(payload.key.public, meta.payload, meta.hash);
-            if (!v) return sec.reject();
-            
-            var ack = createAck(sec.listeners('identify').length);
-            ack.key = payload.key;
-            ack.outgoing = outgoing;
-            ack.payload = payload;
-            
-            ack.on('accept', function () {
-                sec.emit('accept', ack);
-            });
-            
-            ack.on('reject', function () {
-                sec.emit('close');
-            });
-            
-            sec.emit('identify', ack);
-        });
+        stream = through(write, end);
+        stream.id = ack;
         
-        sec.on('pipe', function () {
-            process.nextTick(sendOutgoing);
-        });
-        
-        var outgoing;
-        function sendOutgoing () {
-            outgoing = JSON.stringify({
-                key : {
-                    type : 'rsa',
-                    public : keys.public,
-                },
-                dh : {
-                    group : group,
-                    public : dh.getPublicKey('base64')
-                }
-            });
-            sec.emit('data', JSON.stringify({
-                hash : hash(keys.private, outgoing),
-                payload : outgoing
-            }) + '\n');
+        function write (buf) {
+            var s = encrypt.update(String(pad(buf)));
+            sec.emit('data', frame(keys.private, Buffer(s)));
         }
         
-        if (typeof cb === 'function') sec.on('connection', cb);
-        return sec;
-    };
+        function end () {
+            sec.emit('data', ecrypt.final());
+            sec.emit('end');
+            sec.emit('close');
+        }
+        
+        sec.emit('connection', stream);
+        
+        decrypt = crypto.createDecipher('aes-256-cbc', k);
+        
+        buffers.forEach(function (buf) {
+            var uf = unframe(stream.id.key, buf);
+            if (!uf) {
+                stream.destroy();
+                sec.destroy();
+                return;
+            }
+            stream.emit('data', decrypt.update(uf));
+        });
+        buffers = undefined;
+    });
+    
+    sec.once('header', function (meta) {
+        var payload = JSON.parse(meta.payload);
+        
+        var v = verify(payload.key.public, meta.payload, meta.hash);
+        if (!v) return sec.reject();
+        
+        var ack = createAck(sec.listeners('identify').length);
+        ack.key = payload.key;
+        ack.outgoing = outgoing;
+        ack.payload = payload;
+        
+        ack.on('accept', function () {
+            sec.emit('accept', ack);
+        });
+        
+        ack.on('reject', function () {
+            sec.emit('close');
+        });
+        
+        sec.emit('identify', ack);
+    });
+    
+    sec.on('pipe', function () {
+        process.nextTick(sendOutgoing);
+    });
+    
+    var outgoing;
+    function sendOutgoing () {
+        outgoing = JSON.stringify({
+            key : {
+                type : 'rsa',
+                public : keys.public,
+            },
+            dh : {
+                group : dh.group,
+                public : dh.getPublicKey('base64')
+            }
+        });
+        sec.emit('data', JSON.stringify({
+            hash : hash(keys.private, outgoing),
+            payload : outgoing
+        }) + '\n');
+    }
+    
+    if (typeof cb === 'function') sec.on('connection', cb);
+    return sec;
 };
 
 function zeros (n) {
