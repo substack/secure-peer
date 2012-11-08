e419d52ef5c50388fc200081b233f5848e7fae9a
James Halliday <mail@substack.net>
2012-11-08 08:20:18 -0800

    failing test adding another identify listener

--- test/secure.js
+++ test/secure.js
@@ -1,14 +1,18 @@
 var test = require('tap').test;
+var keys = {
+    a : require('./keys/a.json'),
+    b : require('./keys/b.json'),
+};
 
 var secure = require('../');
 var peer = {
-    a : secure(require('./keys/a.json')),
-    b : secure(require('./keys/b.json')),
+    a : secure(keys.a),
+    b : secure(keys.b),
 };
 var through = require('through');
 
 test('accept a connection', function (t) {
-    t.plan(1);
+    t.plan(3);
     
     var a = peer.a(function (stream) {
         stream.pipe(through(function (buf) {
@@ -25,7 +29,13 @@ test('accept a connection', function (t) {
         stream.end('beep boop');
     });
     
+    a.on('identify', function (id) {
+        t.equal(id.key.public, keys.b.public);
+        id.accept();
+    });
+    
     b.on('identify', function (id) {
+        t.equal(id.key.public, keys.a.public);
         id.accept();
     });
     
