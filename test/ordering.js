bfff302198e6569b4e245e7f246ca1de5947fba8
James Halliday <mail@substack.net>
2012-11-08 17:19:07 -0800

    ordering attack turned into failing test

--- test/ordering.js
+++ test/ordering.js
@@ -13,7 +13,7 @@ var through = require('through');
 var es = require('event-stream');
 
 test('ordering attack', function (t) {
-    t.plan(3);
+    t.plan(5);
     
     var messages = [];
     var msgNum = 0;
@@ -43,24 +43,43 @@ test('ordering attack', function (t) {
         this.emit('end');
     }
     
+    var expected = [ 'beep', ' ', 'boop' ];
+    
     var a = peer.a(function (stream) {
         stream.pipe(through(function (buf) {
+            t.equal(String(buf), expected.shift());
             this.emit('data', String(buf).toUpperCase());
         })).pipe(stream);
     });
     
     var b = peer.b(function (stream) {
-        var data = '';
+        var messages = [];
         stream.on('data', function (buf) {
-            data += buf
+            messages.push(String(buf));
         });
         stream.on('end', function () {
-            console.dir([ 'END', data ]);
+            console.log(
+                'WARNING! REORDERED MESSAGES: '
+                + JSON.stringify(messages)
+            );
+            t.fail('stream should have been destroyed');
+        });
+        
+        stream.on('close', function () {
+            t.ok(true, 'stream was destroyed');
         });
         
         stream.write('beep');
         stream.write(' ');
         stream.end('boop');
+    });
+    
+    b.on('end', function () {
+        t.fail('outer stream should have been destroyed');
+    });
+    
+    b.on('close', function () {
+        t.ok(true, 'outer stream was destroyed');
     });
     
     a.pipe(reorder).pipe(b).pipe(a);
